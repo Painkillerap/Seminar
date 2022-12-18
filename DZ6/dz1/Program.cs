00086e1bcf8a9b1bf05997e24266ce5e6dde5cca
Painkillerap <painkiller.ap@gmail.com>
2022-12-19 00:21:30 +0300

    Seminar6

--- DZ6/dz1/Program.cs
+++ DZ6/dz1/Program.cs
@@ -1,1 +1,68 @@
-﻿
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько 
+чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+int N = InputNumber("Введите количество выводимых цифр: ");
+int SIZE = N;
+const int LEFT_RANGE = -10;
+const int RIGHT_RANGE = 10;
+int [] arr=FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+printArray(arr);
+int PosN=PositivNumber(arr);
+
+System.Console.WriteLine($"Количество цифр больше 0 равно: {PosN}");
+
+
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] tempArr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < tempArr.Length; i++)
+    {
+        tempArr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return tempArr;
+}
+
+int InputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+int PositivNumber(int[] arr)
+{
+    {
+    int count=0;
+    for(int i = 0; i < arr.Length; i++)
+    if(0<arr[i])
+    {
+       count++; 
+    }       
+    return count;
+}
+}
+
+void printArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join("; ", arr) + "]");
+}
--- DZ6/dz2/Program.cs
+++ DZ6/dz2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+int K1 = InputNumber("Введите число K1: ");
+int B1 = InputNumber("Введите число B1: ");
+int K2 = InputNumber("Введите число K2: ");
+int B2 = InputNumber("Введите число B2: ");
+double FuncX = FuncintersectionX(K1, B1, K2, B2);
+double FuncY = FuncintersectionY(K1, B1, FuncX);
+System.Console.WriteLine($"Точка пересечения прямых => ({FuncX}; {FuncY})");
+
+int InputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.WriteLine(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+double FuncintersectionX(double K1, double B1, double K2, double B2)
+{
+    double x = (B2 - B1) / (K1 - K2);
+    return x ;
+}
+
+double FuncintersectionY(double a1, double b1, double x)
+{
+    double y = K1 * FuncX + B1;
+    return y ;
+}
+
